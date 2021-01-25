776966e8724fb71568391befb47291be1f6c43b1
2754github <fkesys@gmail.com>
2021-01-25 14:51:25 +0900

    GH-9: Headerリファクタリング

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -6,12 +6,13 @@ import styles from './Header.module.css';
 
 const Header: FC = () => {
   const router = useRouter();
+  const isHome = router.pathname === '/';
   return (
     <header className={styles.header}>
       <Link href="/">
-        <a className={styles.title}>{router.pathname === '/' ? <h1>{SITE_TITLE}</h1> : SITE_TITLE}</a>
+        <a>{isHome ? <h1>{SITE_TITLE}</h1> : SITE_TITLE}</a>
       </Link>
-      <div className={styles.description}>{SITE_SUBTITLE}</div>
+      <div>{SITE_SUBTITLE}</div>
     </header>
   );
 };
